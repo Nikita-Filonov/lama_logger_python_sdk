55f31597215546ca273fc42a0a0879b2256172ba
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2021-11-19 21:33:05 +0300

    Sdk рефакторинг

--- logger.py
+++ logger.py
@@ -4,18 +4,28 @@ import threading
 import uuid
 from urllib import parse
 
-import requests
-from requests import Response
+import allure
+from requests import Response, post
 
 API_VERSION = 'v1'
-LOGGER_ENDPOINT = os.environ.get('LAMA_LOGGER_ENDPOINT', 'https://lama-logger.herokuapp.com')
+LOGGER_ENDPOINT = os.environ.get('LAMA_LOGGER_ENDPOINT', 'http://localhost:8000')
 
 
 def parse_query_params(url: str) -> dict:
+    """Used to parse query params from url"""
     return dict(parse.parse_qsl(parse.urlsplit(url).query))
 
 
 class LamaLogger:
+    """
+    Base logger class
+
+    Examples:
+        >>> import requests
+        >>> lama_logger = LamaLogger(token='<your_token>', project_id='<your_project_id>')
+        >>> response = requests.get('https://google.com/')
+        >>> lama_logger(response)
+    """
     endpoint = LOGGER_ENDPOINT
     api = f'{LOGGER_ENDPOINT}/api/{API_VERSION}'
 
@@ -27,16 +37,23 @@ class LamaLogger:
         self._context_uuid = None
 
     def __call__(self, response: Response):
-        self.__send_logs(response)
+        request_id = str(uuid.uuid4())
+        setattr(response, 'request_id', request_id)
+        allure.attach(self.link_pattern(request_id), 'Logs', allure.attachment_type.HTML)
+
+        thread = threading.Thread(target=self.send_logs, args=(response,))
+        thread.start()
 
     @property
     def __headers(self):
+        """Headers for send long request"""
         return {
             'Content-Type': 'application/json',
             'Authorization': f'Token {self._token}',
         }
 
     def __to_payload(self, response: Response) -> dict:
+        """Forms json for logger"""
         try:
             response_body = response.json()
         except json.decoder.JSONDecodeError:
@@ -57,22 +74,41 @@ class LamaLogger:
             'nodeId': self._context_uuid
         }
 
-    def __send(self, payload: dict):
-        requests.post(
-            self.api + f'/projects/{self.project_id}/requests/create/',
-            data=json.dumps(payload),
-            headers=self.__headers
-        )
+    def link_pattern(self, request_id: str):
+        """Wrapper for allure to attach like into step"""
+        request_link = self.endpoint + f'/projects/{self.project_id}/?requestId={request_id}'
+        return f'<head></head><body><a href="{request_link}" target="_blank">{request_link}</a></body>'
 
-    def __send_logs(self, response: Response):
+    def send_logs(self, response: Response):
+        """Sending json to logger"""
         payload = self.__to_payload(response)
-        thread = threading.Thread(target=self.__send, args=(payload,))
-        thread.start()
+        endpoint = self.api + f'/projects/{self.project_id}/requests/create/'
+        post(endpoint, data=json.dumps(payload), headers=self.__headers)
 
     def set_context(self, node: str = None):
+        """
+        Should be used to set context
+
+        Example:
+            @pytest.fixture(scope='function', autouse=True)
+            def logger_context(request):
+                logger.set_context(request.node.name)
+                yield
+                logger.drop_context()
+        """
         self._node = node
         self._context_uuid = str(uuid.uuid4())
 
     def drop_context(self):
+        """
+        Should be used to drop context
+
+        Example:
+            @pytest.fixture(scope='function', autouse=True)
+            def logger_context(request):
+                logger.set_context(request.node.name)
+                yield
+                logger.drop_context()
+        """
         self._node = None
         self._context_uuid = None
--- methods.py
+++ methods.py
@@ -1,50 +1,35 @@
-import uuid
-
-import allure
 import requests
 
 from logger import LamaLogger
 
-logger = LamaLogger(token='1ec4ebe3-24ff-4e3d-a750-feb53a5d647b', project_id=1)
-
-
-def link_pattern(request_id):
-    request_link = logger.endpoint + f'/projects/{logger.project_id}/?requestId={request_id}'
-    return f'<head></head><body><a href="{request_link}" target="_blank">{request_link}</a></body>'
-
-
-def response_middleware(response):
-    request_id = str(uuid.uuid4())
-    setattr(response, 'request_id', request_id)
-    allure.attach(link_pattern(request_id), 'Logs', allure.attachment_type.HTML)
-    logger(response)
+logger = LamaLogger(token='2c407474-1911-465a-ae43-01ca1161f33c', project_id=1)
 
 
 def get(url, params=None, **kwargs):
     response = requests.get(url, params=params, stream=True, **kwargs)
-    response_middleware(response)
+    logger(response)
     return response
 
 
 def post(url, data=None, json=None, **kwargs):
     response = requests.post(url, data=data, json=json, stream=True, **kwargs)
-    response_middleware(response)
+    logger(response)
     return response
 
 
 def patch(url, data=None, **kwargs):
     response = requests.patch(url, data=data, stream=True, **kwargs)
-    response_middleware(response)
+    logger(response)
     return response
 
 
 def put(url, data=None, **kwargs):
     response = requests.put(url, data=data, stream=True, **kwargs)
-    response_middleware(response)
+    logger(response)
     return response
 
 
 def delete(url, **kwargs):
     response = requests.delete(url, stream=True, **kwargs)
-    response_middleware(response)
+    logger(response)
     return response
